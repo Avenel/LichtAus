21b538ba667b0ad99a9e50f916d9b0b7821a36a1
Avenel <martin.briewig@googlemail.com>
2015-01-01 19:36:12 +0100

    menü updatet sich nun

--- src/app.js
+++ src/app.js
@@ -6,47 +6,50 @@
 var UI = require('ui');
 var Vector2 = require('vector2');
 var lights = [];
-var lightItems = new Array();
 
 // Get Sensors
-var connection = new WebSocket('ws://192.168.2.21:54322');
-connection.onopen = function () {
-  console.log("send request...")
-  connection.send('dict'); // Send the message 'Ping' to the server
-};
-
-
-connection.onmessage = function(evt) {
-  var parsedString = evt.data.replace(/\'/g, "\"");
-  var data = JSON.parse(parsedString);
-  console.log(data);
-
-  var name;
-  for (name in data) {
-    if (data.hasOwnProperty(name)) {
-      lights.push(name);
+function getMenuItems() {
+  var lightItems = new Array();
+  var connection = new WebSocket('ws://192.168.2.21:54322');
+  connection.onopen = function () {
+    console.log("send request...")
+    connection.send('dict'); // Send the message 'Ping' to the server
+  };
+  
+  connection.onmessage = function(evt) {
+    var parsedString = evt.data.replace(/\'/g, "\"");
+    var data = JSON.parse(parsedString);
+    console.log(data);
+  
+    var name;
+    for (name in data) {
+      if (data.hasOwnProperty(name)) {
+        lights.push(name);
+      }
     }
-  }
-
-  for (var i=0; i<lights.length; i++) {
-    var item;
-    console.log("light: " + lights[i] + ", " + data[lights[i]]);
-    if (data[lights[i]] == "1") {
-      item = {
-        title: lights[i],
-        subtitle: "Licht Aus"
-      };
+  
+    for (var i=0; i<lights.length; i++) {
+      var item;
+      console.log("light: " + lights[i] + ", " + data[lights[i]]);
+      if (data[lights[i]] == "1") {
+        item = {
+          title: lights[i],
+          subtitle: "Licht Aus"
+        };
+      }
+      if (data[lights[i]] == "0") {
+        item = {
+          title: lights[i],
+          subtitle: "Licht An"
+        };
+      }
+      lightItems.push(item);
     }
-    if (data[lights[i]] == "0") {
-      item = {
-        title: lights[i],
-        subtitle: "Licht An"
-      };
-    }
-    lightItems.push(item);
-  }
-
-  console.log(lightItems);
+  
+    console.log(lightItems);
+    var menu =  createMenu(lightItems);
+    menu.show();
+  };
 };
   
 var main = new UI.Card({
@@ -56,12 +59,37 @@ var main = new UI.Card({
   body: 'Drücke Select.'
 });
 
-main.show();
-
 main.on('click', 'select', function(e) {
+  getMenuItems();
+});
+
+main.show();
+  
+var sendCommand = function(cmd) {
+  // Send command
+  var connection = new WebSocket('ws://192.168.2.21:54322');
+  
+  connection.onmessage = function(evt) { 
+    // Show splash
+    console.log("msg received: " + evt.data);
+    var splashCard = new UI.Card({
+      title: "Antwort",
+      body: evt.data
+    });
+    connection.close();
+    getMenuItems();
+  };
+  
+  connection.onopen = function () {
+    connection.send(cmd)
+  };
+  
+};
+
+function createMenu(items) {
   var menu = new UI.Menu({
     sections: [{
-      items: lightItems
+      items: items
     }]
   });
   menu.on('select', function(e) {
@@ -77,26 +105,5 @@ main.on('click', 'select', function(e) {
       sendCommand("Licht Couch An");
     }
   });
-  menu.show();
-});
-  
-var sendCommand = function(cmd) {
-  // Send command
-  var connection = new WebSocket('ws://192.168.2.21:54322');
-  
-  connection.onmessage = function(evt) { 
-    // Show splash
-    console.log("msg received: " + evt.data);
-    var splashCard = new UI.Card({
-      title: "Antwort",
-      body: evt.data
-    });
-    splashCard.show();
-    connection.close();
-  };
-  
-  connection.onopen = function () {
-    connection.send(cmd)
-  };
-  
-};
+  return menu;
+}

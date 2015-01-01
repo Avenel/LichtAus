019552f86f791dad111ec43946788cf16f479962
Avenel <martin.briewig@googlemail.com>
2015-01-01 19:57:22 +0100

    code improvements

--- src/app.js
+++ src/app.js
@@ -4,15 +4,12 @@
  * This is where you write your app.
  */
 var UI = require('ui');
-var Vector2 = require('vector2');
-var lights = [];
 
 // Get Sensors
 function getMenuItems() {
-  var lightItems = new Array();
   var connection = new WebSocket('ws://192.168.2.21:54322');
   connection.onopen = function () {
-    console.log("send request...")
+    console.log("send request...");
     connection.send('dict'); // Send the message 'Ping' to the server
   };
   
@@ -20,6 +17,8 @@ function getMenuItems() {
     var parsedString = evt.data.replace(/\'/g, "\"");
     var data = JSON.parse(parsedString);
     console.log(data);
+    var lightItems = [];
+    var lights = [];
   
     var name;
     for (name in data) {
@@ -30,27 +29,30 @@ function getMenuItems() {
   
     for (var i=0; i<lights.length; i++) {
       var item;
+      if (typeof data[lights[i]] != "string") continue;
+      
       console.log("light: " + lights[i] + ", " + data[lights[i]]);
       if (data[lights[i]] == "1") {
         item = {
           title: lights[i],
           subtitle: "Licht Aus"
         };
+        lightItems.push(item);
       }
       if (data[lights[i]] == "0") {
         item = {
           title: lights[i],
           subtitle: "Licht An"
         };
+        lightItems.push(item);
       }
-      lightItems.push(item);
     }
   
     console.log(lightItems);
     var menu =  createMenu(lightItems);
     menu.show();
   };
-};
+}
   
 var main = new UI.Card({
   title: 'Licht Aus!',
@@ -70,14 +72,10 @@ var sendCommand = function(cmd) {
   var connection = new WebSocket('ws://192.168.2.21:54322');
   
   connection.onmessage = function(evt) { 
-    // Show splash
     console.log("msg received: " + evt.data);
-    var splashCard = new UI.Card({
-      title: "Antwort",
-      body: evt.data
-    });
     connection.close();
-    getMenuItems();
+    // update menu
+    getMenuItems();      
   };
   
   connection.onopen = function () {
